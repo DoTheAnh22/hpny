b119fbae0622c52e6287a18f8d570c9bc80554db
DoTheAnh22 <159463398+DoTheAnh22@users.noreply.github.com>
2024-02-08 23:05:36 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,11 @@
+const nameGirl = 'NocNinn!';
+const giftUrl = 'https://www.facebook.com/gialinh.vuhoang.35';
+const eventName = 'Happy New Yearr';
+const titleCard = 'FORRR YOUUU!';
+const contentCard = 'Chúc embe của anh năm mới luôn bình an, hạnh phúc, luôn tràn ngập niềm vui và những nụ cười. Chúc em luôn vui vẻ và sớm đạt được ước mơ của mình.....sớm gật đầu nhéeee';
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'anhdoita,jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
